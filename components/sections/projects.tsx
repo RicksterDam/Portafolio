4426bfc605893eb116d58f02bfaad61f714c531d
RicksterDam <glezram0409@gmail.com>
2026-06-15 13:02:52 -0600

    Update projects.tsx

--- components/sections/projects.tsx
+++ components/sections/projects.tsx
@@ -140,8 +140,7 @@ export default function Projects() {
                   projects[current]
                 )
               }
-              className="group cursor-pointer rounded-3xl border border-white/10 bg-white/[0.03] p-8 md:p-10 hover:bg-white/[0.05] hover:border-white/20 transition-all duration-300"
-            >
+              className="h-full min-h-[435px] flex flex-col justify-between rounded-3xl border border-white/10 bg-white/[0.03] p-8 hover:bg-white/[0.05] hover:border-white/20 transition-all duration-300"            >
 
               {/* IMAGEN */}
 
