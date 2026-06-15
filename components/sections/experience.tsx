228b7dde52172a24352232169b9cdadff031b383
RicksterDam <glezram0409@gmail.com>
2026-06-15 12:54:58 -0600

    Update experience.tsx

--- components/sections/experience.tsx
+++ components/sections/experience.tsx
@@ -58,7 +58,7 @@ export default function Experience() {
 
     <section
       id="experience"
-      className="w-full"
+      className="h-full min-h-[520px] flex flex-col justify-between rounded-3xl border border-white/10 bg-white/[0.03] p-8 md:p-10 hover:bg-white/[0.05] hover:border-white/20 transition-all duration-300"
     >
 
       <div className="mb-16">
