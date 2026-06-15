30fc711f9f3107ff1e2c586e733e9bbbb7343cd3
RicksterDam <glezram0409@gmail.com>
2026-06-15 12:52:55 -0600

    mod

--- components/sections/projects.tsx
+++ components/sections/projects.tsx
@@ -98,7 +98,7 @@ export default function Projects() {
     <>
       <section
         id="projects"
-        className="w-full"
+        className="h-full min-h-[520px] flex flex-col rounded-3xl border border-white/10 bg-white/[0.03] p-8 hover:bg-white/[0.05] hover:border-white/20 transition-all duration-300"
       >
 
         <div className="mb-16">
